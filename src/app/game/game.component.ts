d07042808ee1835116b88ac3b860dded83f7d264
AlphaCoder147 <thescholar8101@gmail.com>
2023-05-14 21:41:49 +0530

    First Commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent {
+  squares:any = [];
+  xIsNext = true;
+  winner = '';
+  isDraw = '';
+  counter = 0;
+  freshpage = true;
+
+  newgame()
+  {
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.isDraw = '';
+    this.counter = 0;
+    this.freshpage = false;
+  }
+
+  get player()
+  {
+    return this.xIsNext?'X':"O"
+  }
+
+  makeMove(index:number)
+  {
+    if(!this.squares[index])
+    {
+      this.squares.splice(index, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+      this.counter++;
+    }
+    this.winner = this.calculateWinner();
+    if (!this.winner && this.counter == 9)
+    {
+      this.isDraw = 'yes';
+    }
+  }
+
+  calculateWinner()
+  {
+    const lines = [
+      [0,1,2],[3,4,5],[6,7,8],
+      [0,3,6],[1,4,7], [2,5,8],
+      [0,4,8],[2,4,6]
+    ]
+
+    for(let i = 0; i < lines.length; i++)
+    {
+      const [a,b,c] = lines[i];
+      if(this.squares[a] && this.squares[a] === this.squares[b] &&
+         this.squares[a] === this.squares[c])
+         {
+          return this.squares[a];
+         }    
+    }
+    return null; 
+  }
+
+
+}
